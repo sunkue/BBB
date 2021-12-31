1b2d4b10685955978173164bc20ff13298ced4e6
sunkue <wmwwmmm@naver.com>
2021-12-31 19:20:45 +0900

    일단 단순화된 그림자로 끝.

--- BBBClient/BBB/Shader/directional_grass_depthmap_fragment.glsl
+++ BBBClient/BBB/Shader/directional_grass_depthmap_fragment.glsl
@@ -1,13 +1,13 @@
 #version 450
 
-uniform sampler2D u_tex_sampler[4];
+//uniform sampler2D u_tex_sampler[4];
 
-flat in int texture_index;
-in vec2 texcoord;
+//flat in int texture_index;
+//in vec2 texcoord;
 
 void main()
 {
-	float alpha = texture(u_tex_sampler[texture_index], texcoord).a;
+//	float alpha = texture(u_tex_sampler[texture_index], texcoord).a;
 //	if (alpha < 0.1)
 //		discard;
 //  if 최적화 엄청 심하네,, 그림자맵 해상도에 비례해서 더 그럼..
